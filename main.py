eee8ea326cd455399df94f292179d5f0cb845293
Your Name <mkim01@stuy.edu>
2020-02-11 00:07:52 -0500

    done

--- main.py
+++ main.py
@@ -2,36 +2,28 @@ from display import *
 from draw import *
 
 s = new_screen()
-c = [ 0, 255, 0 ]
+#outline
+r = 240
+g = 1
+b = 1
+c = [r , g , b]
+for i in range (15):
+    c = [r , g , b]
+    g += 15
+    b += 15
+    draw_line(XRES / 2 + i, 100 + i, XRES/2 + i, YRES/2 + i , s, c)
+    draw_line(XRES / 2 + i, 100 + i, XRES / 2 + 125 + i, 150 + i ,  s, c)
+    draw_line(XRES/2 + i , YRES/2 + i , XRES/2 + 125 + i , YRES/2 + 50 +i  , s, c)
+    draw_line(XRES / 2 + 125 + i , 150 + i , XRES/2 + 125 + i , YRES/2 + 50 + i, s, c)
+    draw_line(XRES / 2 + i , 100 + i , XRES / 2 - 125 + i , 150 + i, s, c)
+    draw_line(XRES/2 - 125 + i , YRES/2 + 50 + i, XRES / 2 - 125 + i, 150 + i, s, c )
+    draw_line(XRES/2 - 125 + i, YRES/2 + 50 + i, XRES/2 +i , YRES/2 + i , s, c )
+    draw_line(XRES/2 + 125 + i , YRES/2 + 50 + i, XRES/2 + i  , YRES/2 + 100 + i, s, c)
+    draw_line(XRES/2 + i, YRES/2 + 100 + i , XRES / 2 - 125 + i , YRES/2 + 50 +i , s, c)
 
-#octants 1 and 5
-draw_line(0, 0, XRES-1, YRES-1, s, c)
-draw_line(0, 0, XRES-1, YRES / 2, s, c)
-draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
 
-#octants 8 and 4
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES-1, 0, s, c);
-draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-draw_line(XRES-1, 0, 0, YRES/2, s, c);
 
-#octants 2 and 6
-c[RED] = 255;
-c[GREEN] = 0;
-c[BLUE] = 0;
-draw_line(0, 0, XRES/2, YRES-1, s, c);
-draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
 
-#octants 7 and 3
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES/2, 0, s, c);
-draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
-
-#horizontal and vertical
-c[BLUE] = 0;
-c[GREEN] = 255;
-draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
-draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 
 
 display(s)
